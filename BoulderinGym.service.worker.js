721e2afa06ac28b343fcf56a0cedf3169d9d25fb
André Ferraz <thebasdagger@gmail.com>
2023-01-03 01:34:22 +0100

    added key

--- BoulderinGym.service.worker.js
+++ BoulderinGym.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1672704727|166892032";
+const CACHE_VERSION = "1672705971|1410704884";
 const CACHE_PREFIX = "BoulderinGym-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "BoulderinGym.offline.html";
